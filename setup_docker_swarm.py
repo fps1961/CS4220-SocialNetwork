ba8e97f87a9eac4c5f64bd15e2ea00cce6535478
FruitPunchSamurai <sheshan1961@gmail.com>
2024-10-16 15:37:52 -0400

    comment

--- setup_docker_swarm.py
+++ setup_docker_swarm.py
@@ -1,6 +1,5 @@
 import argparse
 import os
-import re
 import shlex
 import subprocess
 from pathlib import Path
@@ -48,90 +47,91 @@ args = parse_args()
 
 with ThreadingGroup(*[f'node-{idx}' for idx in range(0, args.number)]) as swarm_grp, \
         ThreadingGroup(*[f'node-{idx}' for idx in range(args.number, args.number + args.client_number)]) as client_grp:
-    swarm_grp.run(delete_repo)
+    # swarm_grp.run(delete_repo)
+    #
+    #
+    # def stop_swarm_cluster():
+    #     swarm_grp.run('sudo docker swarm leave')
+    #     subprocess.run(shlex.split('sudo docker swarm leave -f'))
+    #
+    #
+    # def clear_env():
+    #     swarm_grp.run(
+    #         'sudo apt-get -y purge docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extras')
+    #     swarm_grp.run(
+    #         'for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done')
+    #     swarm_grp.run('sudo rm -rf /var/lib/containerd')
+    #     swarm_grp.run('sudo rm -rf /var/lib/docker')
+    #     swarm_grp.run('sudo rm /etc/apt/keyrings/docker.gpg')
+    #
+    #
+    #
+    # swarm_grp.run(install_collectl)
+    # print('** collectl installed **')
+    # swarm_grp.run(clone_official_socialnetwork_repo)
+    # print('** socialNetwork cloned **')
+    #
+    # swarm_grp.run(install_docker)
+    # print('** docker installed **')
+    #
+    # # Initialize Docker Swarm
+    # ret = subprocess.run(['sudo', 'docker', 'swarm', 'init', '--advertise-addr', args.ip], capture_output=True)
+    # print('** swarm manager initialized **')
+    # swarm_join_cmd_ptn = r'(docker swarm join --token .*:\d+)'
+    # swarm_join_cmd = re.search(swarm_join_cmd_ptn, ret.stdout.decode('utf-8'))
+    # if swarm_join_cmd is None:
+    #     print('Didn\'t find match pattern for docker swarm join:')
+    #     print(f'The ret of swarm init is {ret}')
+    #     exit(0)
+    # else:
+    #     swarm_join_cmd = swarm_join_cmd.group()
+    # with ThreadingGroup.from_connections(swarm_grp[1:]) as grp_worker:
+    #     grp_worker.run('sudo ' + swarm_join_cmd)
+    #     grp_worker.run(
+    #         'if [ ! -e "$HOME/.ssh/config" ]; then echo -e "Host *\n\tStrictHostKeyChecking no" >> $HOME/.ssh/config; fi')
+    #     grp_worker.put(Path.home() / '.ssh' / 'id_rsa', '.ssh')
+    # print('** swarm cluster ready **')
+    #
+    # subprocess.run(shlex.split('sudo docker service create --name registry \
+    #                            --publish published=5000,target=5000 registry:2'))
+    # print('** registry service created **')
+    #
+    # client_grp[0].run(install_sysdig)
+    # client_grp.put(Path.home() / '.ssh/id_rsa', '.ssh')
+    # client_grp.run(
+    #     'if [ ! -e "$HOME/.ssh/config" ]; then echo -e "Host *\n\tStrictHostKeyChecking no" >> $HOME/.ssh/config; fi')
+    # client_grp.put(Path.home() / 'RubbosClient.zip')
+    # client_grp.run('unzip RubbosClient.zip')
+    # client_grp.run('mv RubbosClient/elba .')
+    # client_grp.run('mv RubbosClient/rubbos .')
+    # client_grp.run('gcc $HOME/elba/rubbos/RUBBoS/bench/flush_cache.c -o $HOME/elba/rubbos/RUBBoS/bench/flush_cache')
+    # print('** RubbosClient copied **')
+    #
+    # os.chdir(Path.home())
+    # for file in ['src', 'RubbosClient_src', 'socialNetwork', 'scripts_limit', 'internal_triggers']:
+    #     subprocess.run(shlex.split(f'unzip {file}.zip'))
+    # subprocess.run(shlex.split('mv DeathStarBench/socialNetwork/src DeathStarBench/socialNetwork/src.bk'))
+    # subprocess.run(shlex.split('mv src DeathStarBench/socialNetwork/'))
+    # os.chdir(Path.home() / 'DeathStarBench' / 'socialNetwork')
+    # subprocess.run(shlex.split('sudo docker build -t 127.0.0.1:5000/social-network-microservices:withLog_01 .'))
+    # subprocess.run(shlex.split('sudo docker push 127.0.0.1:5000/social-network-microservices:withLog_01'))
+    # os.chdir(Path.home() / 'internal_triggers' / 'cpu')
+    # subprocess.run(shlex.split('sudo docker build -t 127.0.0.1:5000/cpu_intensive .'))
+    # subprocess.run(shlex.split('sudo docker push 127.0.0.1:5000/cpu_intensive'))
+    # os.chdir(Path.home() / 'internal_triggers' / 'io')
+    # subprocess.run(shlex.split('sudo docker build -t 127.0.0.1:5000/io_intensive .'))
+    # subprocess.run(shlex.split('sudo docker push 127.0.0.1:5000/io_intensive'))
+    # print('** customized socialNetwork docker images built **')
+    #
+    # os.chdir(Path.home())
+    # subprocess.run('mv socialNetwork/* DeathStarBench/socialNetwork/', shell=True)
+    # subprocess.run('mv socialNetwork/scripts/*.sh DeathStarBench/socialNetwork/scripts/', shell=True)
+    # subprocess.run(shlex.split('rm -r socialNetwork/scripts'))
+    # os.chdir(Path.home() / 'DeathStarBench' / 'socialNetwork')
+    # subprocess.run(shlex.split('sudo ./start.sh start'))
+    # print('** socialNetwork stack deployed **')
 
-
-    def stop_swarm_cluster():
-        swarm_grp.run('sudo docker swarm leave')
-        subprocess.run(shlex.split('sudo docker swarm leave -f'))
-
-
-    def clear_env():
-        swarm_grp.run(
-            'sudo apt-get -y purge docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extras')
-        swarm_grp.run(
-            'for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done')
-        swarm_grp.run('sudo rm -rf /var/lib/containerd')
-        swarm_grp.run('sudo rm -rf /var/lib/docker')
-        swarm_grp.run('sudo rm /etc/apt/keyrings/docker.gpg')
-
-
-
-    swarm_grp.run(install_collectl)
-    print('** collectl installed **')
-    swarm_grp.run(clone_official_socialnetwork_repo)
-    print('** socialNetwork cloned **')
-
-    swarm_grp.run(install_docker)
-    print('** docker installed **')
-
-    # Initialize Docker Swarm
-    ret = subprocess.run(['sudo', 'docker', 'swarm', 'init', '--advertise-addr', args.ip], capture_output=True)
-    print('** swarm manager initialized **')
-    swarm_join_cmd_ptn = r'(docker swarm join --token .*:\d+)'
-    swarm_join_cmd = re.search(swarm_join_cmd_ptn, ret.stdout.decode('utf-8'))
-    if swarm_join_cmd is None:
-        print('Didn\'t find match pattern for docker swarm join:')
-        print(f'The ret of swarm init is {ret}')
-        exit(0)
-    else:
-        swarm_join_cmd = swarm_join_cmd.group()
-    with ThreadingGroup.from_connections(swarm_grp[1:]) as grp_worker:
-        grp_worker.run('sudo ' + swarm_join_cmd)
-        grp_worker.run(
-            'if [ ! -e "$HOME/.ssh/config" ]; then echo -e "Host *\n\tStrictHostKeyChecking no" >> $HOME/.ssh/config; fi')
-        grp_worker.put(Path.home() / '.ssh' / 'id_rsa', '.ssh')
-    print('** swarm cluster ready **')
-
-    subprocess.run(shlex.split('sudo docker service create --name registry \
-                               --publish published=5000,target=5000 registry:2'))
-    print('** registry service created **')
-
-    client_grp[0].run(install_sysdig)
-    client_grp.put(Path.home() / '.ssh/id_rsa', '.ssh')
-    client_grp.run(
-        'if [ ! -e "$HOME/.ssh/config" ]; then echo -e "Host *\n\tStrictHostKeyChecking no" >> $HOME/.ssh/config; fi')
-    client_grp.put(Path.home() / 'RubbosClient.zip')
-    client_grp.run('unzip RubbosClient.zip')
-    client_grp.run('mv RubbosClient/elba .')
-    client_grp.run('mv RubbosClient/rubbos .')
-    client_grp.run('gcc $HOME/elba/rubbos/RUBBoS/bench/flush_cache.c -o $HOME/elba/rubbos/RUBBoS/bench/flush_cache')
-    print('** RubbosClient copied **')
-
-    os.chdir(Path.home())
-    for file in ['src', 'RubbosClient_src', 'socialNetwork', 'scripts_limit', 'internal_triggers']:
-        subprocess.run(shlex.split(f'unzip {file}.zip'))
-    subprocess.run(shlex.split('mv DeathStarBench/socialNetwork/src DeathStarBench/socialNetwork/src.bk'))
-    subprocess.run(shlex.split('mv src DeathStarBench/socialNetwork/'))
-    os.chdir(Path.home() / 'DeathStarBench' / 'socialNetwork')
-    subprocess.run(shlex.split('sudo docker build -t 127.0.0.1:5000/social-network-microservices:withLog_01 .'))
-    subprocess.run(shlex.split('sudo docker push 127.0.0.1:5000/social-network-microservices:withLog_01'))
-    os.chdir(Path.home() / 'internal_triggers' / 'cpu')
-    subprocess.run(shlex.split('sudo docker build -t 127.0.0.1:5000/cpu_intensive .'))
-    subprocess.run(shlex.split('sudo docker push 127.0.0.1:5000/cpu_intensive'))
-    os.chdir(Path.home() / 'internal_triggers' / 'io')
-    subprocess.run(shlex.split('sudo docker build -t 127.0.0.1:5000/io_intensive .'))
-    subprocess.run(shlex.split('sudo docker push 127.0.0.1:5000/io_intensive'))
-    print('** customized socialNetwork docker images built **')
-
-    os.chdir(Path.home())
-    subprocess.run('mv socialNetwork/* DeathStarBench/socialNetwork/', shell=True)
-    subprocess.run('mv socialNetwork/scripts/*.sh DeathStarBench/socialNetwork/scripts/', shell=True)
-    subprocess.run(shlex.split('rm -r socialNetwork/scripts'))
-    os.chdir(Path.home() / 'DeathStarBench' / 'socialNetwork')
-    subprocess.run(shlex.split('sudo ./start.sh start'))
-    print('** socialNetwork stack deployed **')
-
+    subprocess.run(shlex.split(f"chmod -R +x {Path.home()}/DeathStarBench/"))
     os.chdir(Path.home() / 'RubbosClient_src')
     subprocess.run(shlex.split('mvn clean'))
     subprocess.run(shlex.split('mvn package'))
