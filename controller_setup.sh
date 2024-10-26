f323e5936c736f5dbde8fb12eb0136b0daee4a34
FruitPunchSamurai <sheshan1961@gmail.com>
2024-10-25 20:26:11 -0400

    comment

--- controller_setup.sh
+++ controller_setup.sh
@@ -105,6 +105,6 @@ ssh -o StrictHostKeyChecking=no -i ${private_ssh_key_path} ${username}@${control
   sudo cp /users/${username}/scripts_limit/generateResult.sh /users/${username}/socialNetwork/
   sudo apt install -y python2
   cd /users/${username}/socialNetwork/
-  chmod -R +x ./generateResult.sh
+  sudo chmod -R +x ./generateResult.sh
   ./generateResult.sh &> output.log
 "
